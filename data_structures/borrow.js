729d1a73bb1df4e527ce3eb6d5c2d72394be2900
liangyu <liangyu@wead.mobi>
2018-10-24 14:09:35 +0800

    save

--- data_structures/borrow.js
+++ data_structures/borrow.js
@@ -19,3 +19,24 @@ movies.forEach((item) => {
   movieList.append(item)
 })
 console.log(movieList.dataStore)
+// let names = new List()
+// names.append('nrf')
+// names.append('dqf')
+// names.append('wch')
+// console.log(names.toString())
+// names.end()
+// console.log(names.getElement())
+// names.prev()
+// console.log(names.getElement())
+// names.next()
+// console.log(names.getElement())
+// names.font()
+// console.log(names.getElement())
+// for (names.font(); names.hasNext(); names.next()) { // 迭代器顺序遍历
+//   console.log(`name: ${names.getElement()}`)
+// }
+// for (names.end(); names.hasPrev(); names.prev()) { // 迭代器倒序遍历
+//   console.log(`name: ${names.getElement()}`)
+// }
+// names.remove('wch')
+// console.log(names.toString())
--- data_structures/list.js
+++ data_structures/list.js
@@ -191,26 +191,4 @@ function hasPrev () {
   return self.pos >= 0
 }
 
-// let names = new List()
-// names.append('nrf')
-// names.append('dqf')
-// names.append('wch')
-// console.log(names.toString())
-// names.end()
-// console.log(names.getElement())
-// names.prev()
-// console.log(names.getElement())
-// names.next()
-// console.log(names.getElement())
-// names.font()
-// console.log(names.getElement())
-// for (names.font(); names.hasNext(); names.next()) { // 迭代器顺序遍历
-//   console.log(`name: ${names.getElement()}`)
-// }
-// for (names.end(); names.hasPrev(); names.prev()) { // 迭代器倒序遍历
-//   console.log(`name: ${names.getElement()}`)
-// }
-// names.remove('wch')
-// console.log(names.toString())
-
 exports.List = List;
