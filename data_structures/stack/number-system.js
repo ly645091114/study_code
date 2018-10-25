dd9f4915dc5afdaa76c17270e1cb5c0d511345e4
liangyu <liangyu@wead.mobi>
2018-10-25 13:48:56 +0800

    栈的运用

---
+++ data_structures/stack/number-system.js
@@ -0,0 +1,19 @@
+let { Stack } = require('./stack.js')
+/**
+ * 该方法，通过栈的形式，实现了十进制数制的转换，但仅限于基数为 2 ~ 9的数制
+ * @param { Number } num 十进制数
+ * @param { Number } base 基数
+ */
+function mulBase (num, base) {
+  let s = new Stack()
+  do {
+    s.push(num % base)
+    num = Math.floor(num /= base)
+  } while (num > 0)
+  let converted = ''
+  while (s.length() > 0) {
+    converted += s.pop()
+  }
+  return converted
+}
+exports.mulBase = mulBase
---
+++ data_structures/stack/stack.js
@@ -0,0 +1,44 @@
+/**
+ * 构造一个 stack 类
+ */
+function Stack () {
+  this.dataStore = []
+  this.top = 0
+  this.push = push
+  this.pop = pop
+  this.peek = peek
+  this.length = length
+  this.clear = clear
+}
+/**
+ * 入栈方法
+ * @param { Any } element 入栈元素
+ */
+function push (element) {
+  this.dataStore[this.top++] = element
+}
+/**
+ * 出栈方法
+ */
+function pop () {
+  return this.dataStore[--this.top]
+}
+/**
+ * 输出栈顶元素
+ */
+function peek () {
+  return this.dataStore[this.top - 1]
+}
+/**
+ * 获取栈内元素
+ */
+function length () {
+  return this.top
+}
+/**
+ * 清空栈
+ */
+function clear () {
+  return this.top = 0
+}
+exports.Stack = Stack
