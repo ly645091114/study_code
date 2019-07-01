d13cda3daa04ba53f39092ebd6dd13bb6599b2a4
liangyu <liangyu@wead.mobi>
2019-07-01 22:17:43 +0800

    安装模板依赖包

--- node/study_node/10-http-template.js
+++ node/study_node/10-http-template.js
@@ -6,7 +6,6 @@ let server = http.createServer()
 let wwwDir = './'
 
 server.on('request', function (req, res) {
-  let url = req.url
   /**
    * 1.如何得到 wwwDir 目录列表中的文件名和目录名
    *  fs.readdir
@@ -25,10 +24,12 @@ server.on('request', function (req, res) {
       // 生成需要替换的内容
       let content = ''
       files.forEach(function (item) {
+        let fileInfo = fs.statSync(`${wwwDir}${item}`)
         content += `
           <tr>
-            <td data-value="${item}"><a class="icon dir" href="${wwwDir}/${item}">${item}</a></td><td class="detailsColumn" data-value="0"></td>
-            <td class="detailsColumn" data-value="1524147222">2018/4/19 下午10:13:42</td>
+            <td data-value="${item}"><a class="icon ${fileType(fileInfo)}" href="${wwwDir}/${item}">${item}</a></td>
+            <td class="detailsColumn" data-value="${fileInfo.size}">${fileInfo.size} B</td>
+            <td class="detailsColumn" data-value="1524147222">${formatTime(fileInfo.mtime)}</td>
           </tr>`
       })
 
@@ -45,3 +46,38 @@ server.on('request', function (req, res) {
 server.listen(8086, function () {
   console.log('Server is running ...')
 })
+
+/**
+ * @description 时间格式化
+ * @param { Any } time 格式化时间
+ * @returns { String } 格式化日期
+ */
+const formatTime = function (time) {
+  let date = new Date(time)
+  let YYYY = date.getFullYear()
+  let MM = date.getMonth() + 1
+  let DD = date.getDate()
+  let HH = date.getHours()
+  let mm = date.getMinutes()
+  let ss = date.getSeconds()
+  return `${YYYY}/${doubleNum(MM)}/${doubleNum(DD)} ${doubleNum(HH)}:${doubleNum(mm)}:${doubleNum(ss)}`
+}
+
+/**
+ * @description 个位数的值在前方加 0 零
+ * @param { Number } num 处理参数
+ * @return { String } 格式化参数
+ */
+const doubleNum = function (num) {
+  return num < 10 ? `0${num}` : String(num)
+}
+
+/**
+ * @description 判断文件类型
+ * @param { Object } 文件属性对象
+ * @returns { String } 返回图标类
+ */
+const fileType = function (stats) {
+  let isFile = stats.isFile()
+  return isFile ? 'file' : 'dir'
+}
